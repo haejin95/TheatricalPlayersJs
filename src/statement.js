26acd50291a2d54a27d52cc9dda03feac0a3545a
Hazle <parkheajin39@naver.com>
2023-03-29 07:16:26 +0900

    임시변수를 질의 함수로 바꾸기 & 변수 쉽게 만들기

--- src/statement.js
+++ src/statement.js
@@ -7,26 +7,29 @@ function statement (invoice, plays) {
         { style: "currency", currency: "USD",
             minimumFractionDigits: 2 }).format;
 
-    for (let perf of invoice.performances) {
-        const play = plays[perf.playID];
-        let thisAmount = getAmount(perf.audience, play.type);
+    for (let perf of invoice.performances) {g
+        let thisAmount = getAmount(perf);
 
-        // add volume credits
         volumeCredits += Math.max(perf.audience - 30, 0);
-        // add extra credit for every ten comedy attendees
-        if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+        if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
         // print line for this order
-        result += ` ${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
+        result += ` ${playFor(perf).name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
         totalAmount += thisAmount;
     }
     result += `Amount owed is ${format(totalAmount/100)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
     return result;
 }
+//2 임시변수를 질의 함수로 바꾸기 & 변수 쉽게 만들기
+function playFor(performance) {
+    return plays[performance.playID];
+}
 
 //1 함수 추출
-function getAmount(audience, playType) {
-    let result;
+function getAmount(performance) {
+    let audience = performance.audience;
+    let playType = playFor(performance).type;
+    let result = 0;
     switch (playType) {
         case "tragedy":
             result = 40000;
@@ -36,7 +39,7 @@ function getAmount(audience, playType) {
             break;
         case "comedy":
             result = 30000;
-            if (audience > 20) {
+            if (performance.audience > 20) {
                 result += 10000 + 500 * (audience - 20);
             }
             result += 300 * audience;
