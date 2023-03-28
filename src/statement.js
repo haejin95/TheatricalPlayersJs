9e0fc7bc10bab718d8d5db53d8298894a85a5da3
Hazle <parkheajin39@naver.com>
2023-03-29 07:06:50 +0900

    함수 추출하기

--- src/statement.js
+++ src/statement.js
@@ -9,24 +9,8 @@ function statement (invoice, plays) {
 
     for (let perf of invoice.performances) {
         const play = plays[perf.playID];
-        let thisAmount = 0;
-        switch (play.type) {
-            case "tragedy":
-                thisAmount = 40000;
-                if (perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience - 30);
-                }
-                break;
-            case "comedy":
-                thisAmount = 30000;
-                if (perf.audience > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20);
-                }
-                thisAmount += 300 * perf.audience;
-                break;
-            default:
-                throw new Error(`unknown type: ${play.type}`);
-        }
+        let thisAmount = getAmount(perf.audience, play.type);
+
         // add volume credits
         volumeCredits += Math.max(perf.audience - 30, 0);
         // add extra credit for every ten comedy attendees
@@ -37,6 +21,29 @@ function statement (invoice, plays) {
     }
     result += `Amount owed is ${format(totalAmount/100)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
+    return result;
+}
+
+//1 함수 추출
+function getAmount(audience, playType) {
+    let result;
+    switch (playType) {
+        case "tragedy":
+            result = 40000;
+            if (audience > 30) {
+                result += 1000 * (audience - 30);
+            }
+            break;
+        case "comedy":
+            result = 30000;
+            if (audience > 20) {
+                result += 10000 + 500 * (audience - 20);
+            }
+            result += 300 * audience;
+            break;
+        default:
+            throw new Error(`unknown type: ${playType}`);
+    }
     return result;
 }
 
